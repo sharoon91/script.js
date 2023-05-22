e39babbc5e403b172dafe4549a10959452b981a4
Sharoon Kumar <134275580+sharoon91@users.noreply.github.com>
2023-05-22 14:40:31 +0500

    one.js

--- one.js
+++ one.js
@@ -1,8 +1,7 @@
-var firstNameInput = document.querySelector('#text-00000012');
+// Replace "field1" and "field2" with the actual field IDs or names of your form fields
+var nameField = document.getElementById('field1');
+var displayField = document.getElementById('field2');
 
-var firstNameLabel = document.querySelector('#text-0000001e');
-
-firstNameInput.addEventListener('input', function() {
-  // Update the label text with the entered value
-  firstNameLabel.value =  firstNameInput.value;
+nameField.addEventListener('input', function() {
+  displayField.value = nameField.value;
 });
