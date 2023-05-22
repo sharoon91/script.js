f68c2df2c1467d0c896e4183dd41246e312efacb
Sharoon Kumar <134275580+sharoon91@users.noreply.github.com>
2023-05-22 15:06:15 +0500

    one.js

--- one.js
+++ one.js
@@ -1,6 +1,6 @@
 // Replace "field1" and "field2" with the actual field IDs or names of your form fields
-var nameField = document.getElementById('field1');
-var displayField = document.getElementById('field2');
+var nameField = document.getElementById('First-Name');
+var displayField = document.getElementById('Short-Answer');
 
 nameField.addEventListener('input', function() {
   displayField.value = nameField.value;
